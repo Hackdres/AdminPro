3072e533eddd04628b04c8251d8bb6d0e8b96c9a
Andres <hackdres@outlook.com>
2018-10-31 16:09:22 -0500

    AdminPro Base

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+
+declare function init_plugins();
+declare const gapi: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email: string;
+  recuerdame: boolean = false;
+
+  auth2: any;
+
+  constructor(
+    public router: Router,
+    public _usuarioService: UsuarioService
+    ) { }
+
+  ngOnInit() {
+    init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+    if ( this.email.length > 1 ) {
+      this.recuerdame = true;
+    }
+
+  }
+
+  googleInit() {
+
+    gapi.load('auth2', () => {
+
+      this.auth2 = gapi.auth2.init({
+        client_id: '812319599576-5593gfffooup3fa5m1871c6tter6kb8a.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin( document.getElementById('btnGoogle') );
+
+    });
+
+  }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler( element, {}, (googleUser) => {
+
+      // let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle( token )
+        // .subscribe( () => this.router.navigate(['/dashboard']) );
+        .subscribe( () => window.location.href = '#/dashboard' );
+
+    });
+
+  }
+
+  ingresar( forma: NgForm) {
+
+    if (forma.invalid){
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login( usuario, forma.value.recuerdame )
+      .subscribe( correcto => this.router.navigate(['/dashboard']) );
+
+    // this.router.navigate([ '/dashboard' ]);
+
+  }
+
+}
